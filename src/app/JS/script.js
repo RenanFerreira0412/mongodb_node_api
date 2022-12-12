eefe9bd892b01457ca8c54dd85cff6f10797c828
RenanFerreira0412 <renan_igor03@hotmail.com>
2022-12-12 18:21:18 -0300

    Finalização da autenticação dos usuários

--- src/app/JS/script.js
+++ src/app/JS/script.js
@@ -6,17 +6,24 @@ $(document).ready(function () {
     var citiesList = [];
     var statesList = [];
 
+    //testa se existe o item token no localStorage do navegador.
+    if (localStorage.getItem("token") == null) {
+
+        //Armazenamento local dos tokens 
+        localStorage.setItem("token", JSON.stringify([]));
+    }
+
     var dados = {
         "isLogin": true,
+        "isAuthenticated": localStorage.getItem("token").length <= 2 ? false : true,
         "user": user,
-        "new_user": { name: "", email: "", password: "", confirmPassword: "", city: "", state: "" },
+        "register": { name: "", email: "", password: "", confirmPassword: "", city: "", state: "" },
         "login": { email: "", password: "" },
         "activity": activity,
-        "new_activity": { name: "", deadline: "", subject: {} },
+        "new_activity": { _id: "", name: "", deadline: "", subject: {} },
         "subject": subject,
-        "new_subject": { name: "", teacher: "" },
+        "new_subject": { _id: "", name: "", teacher: "" },
         "authPageText": { title: "", actionButton1: "", actionButton2: "", toggleButton: "" },
-        "localidade": { _myState: "", _myCity: "" },
         "citiesList": citiesList,
         "statesList": statesList,
         "stateInfoUrl": "https://servicodados.ibge.gov.br/api/v1/localidades/estados?orderBy=nome"
@@ -27,7 +34,7 @@ $(document).ready(function () {
     // global
     Vue.use(window.vuelidate.default);
 
-    const { required, maxLength, minValue, numeric } = window.validators
+    const { required, maxLength, minLength, numeric, sameAs } = window.validators
 
     new Vue({
         el: '#app',
@@ -35,20 +42,47 @@ $(document).ready(function () {
         validations: {
             login: {
                 email: {
+                    required,
+                },
+                password: {
+                    required
+                }
+            },
+            register: {
+                name: {
+                    required,
+                    minLength: minLength(6)
+                },
+                email: {
                     required
                 },
                 password: {
                     required,
-                    minValue: minValue(6)
+                    minLength: minLength(6)
+                },
+                confirmPassword: {
+                    required,
+                    sameAsPassword: sameAs('password')
+                },
+                city: {
+                    required
+                },
+                state: {
+                    required
                 }
             }
         },
         created: function () {
             this.getStateList();
             this.setFormAction(true);
-            console.log(this.citiesList)
         },
         methods: {
+            status: function (validation) {
+                return {
+                    error: validation.$error,
+                    dirty: validation.$dirty
+                }
+            },
             setFormAction: function (acao) {
                 this.isLogin = acao;
                 if (this.isLogin) {
@@ -65,8 +99,8 @@ $(document).ready(function () {
             },
 
             updateState: function (state) {
-                this.localidade._myState = state;
-                this.localidade._myCity = '';
+                this.register.state = state;
+                this.register.city = '';
                 this.citiesList = []
                 console.log(state)
 
@@ -74,9 +108,9 @@ $(document).ready(function () {
             },
 
             updateCity: function (city) {
-                this.localidade._myCity = city;
+                this.register.city = city;
                 console.log(city)
-                console.log(this.localidade._myCity)
+                console.log(this.register.city)
             },
 
             getStateList: async function () {
@@ -108,9 +142,92 @@ $(document).ready(function () {
                     alert('Erro ao carregar as cidades: ' + error.response.data);
                 });
             },
-            login: function (v) {
+            authButton: function (v) {
+                //console.log(v.login)
+                var login = jQuery.extend({}, this.login);
+                var register = jQuery.extend({}, this.register);
 
+                //Verifica se está no formulário de login 
+                if (this.isLogin) {
+                    if (!v.login.$invalid) { //Realiza o teste de validação
+                        this.$http.post('http://localhost:3000/login', login)
+                            .then(response => {
+                                if (response.data.length != 0) {
+                                    console.log('Acesso permitido')
+                                    for (let user of response.data) {
+                                        console.log(user._id)
+                                        this.user.push(user);
+
+                                        //Adiciona no localStorage o token de acesso do usuário
+                                        localStorage.setItem("token", JSON.stringify(user._id));
+                                    }
+
+                                    //Limpa o formulário
+                                    this.cleanFormLogin();
+
+                                    //Habilita o acesso a homepage do site
+                                    this.isAuthenticated = true;
+                                } else {
+                                    console.log('Acesso negado')
+                                    alert("Verifique se o email e a senha estão corretos")
+                                }
+                            }).catch(response => {
+                                // error callback
+                                alert('Não foi possível logar o usuário');
+                                console.log(response);
+                            });
+                    } else {
+                        alert('Preencha todos os campos corretamente !!!')
+                    }
+                } else { //Caso contrário, o usuário está no formulário de cadastro
+                    if (!v.register.$invalid) { //Teste de validação
+                        this.$http.post('http://localhost:3000/register', register)
+                            .then(response => {
+                                console.log(response.data)
+                                this.user.push(response.data);
+
+                                //Adiciona no localStorage o token de acesso do usuário
+                                localStorage.setItem("token", JSON.stringify(response.data._id));
+
+                                //Limpa o formulário
+                                this.cleanFormRegister()
+
+                                //Habilita o acesso a homepage do site
+                                this.isAuthenticated = true;
+                            }).catch(response => {
+                                // error callback
+                                alert('Não foi possível cadastrar o usuário');
+                                console.log(response);
+                            });
+                    } else {
+                        alert('Preencha todos os campos corretamente !!!')
+                    }
+                }
+            },
+            logout: function () {
+                const logout = confirm('Desejas sair do app?');
+
+                if (logout) {
+                    //Exclui o token do localStorage
+                    localStorage.setItem("token", JSON.stringify([]));
+
+                    //Desabilita o acesso a homepage do site
+                    this.isAuthenticated = false;
+                }
+            },
+            cleanFormLogin: function () {
+                this.login.email = '';
+                this.login.password = '';
+            },
+            cleanFormRegister: function () {
+                this.register.name = '';
+                this.register.email = '';
+                this.register.password = '';
+                this.register.confirmPassword = '';
+                this.register.state = '';
+                this.register.city = '';
             }
+
         }
     })
 });
--- src/index.ts
+++ src/index.ts
@@ -9,11 +9,12 @@ import setup from "./database/connect"
 const app = express();
 app.use(express.json());
 
-app.use(
-    express.urlencoded({
-        extended: true,
-    }),
-)
+app.use(function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+    res.header('Access-Control-Allow-Methods', 'GET,POST');
+    next();
+});
 
 //Utilização das rotas definidas
 app.use(routes);
